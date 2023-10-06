9c1672cb730bbce9e0c4d33a9dee6017d1044a8d
Dagmawi Beyene <73708647+Dagmawi-Beyene@users.noreply.github.com>
2023-10-06 17:58:29 +0300

    AI commit

---
+++ .env/.dev.ts
@@ -0,0 +1,8 @@
+export const env = {
+    account: process.env.AWS_ACCOUNT_ID || 'account',
+    region: process.env.AWS_REGION || 'eu-west-1'
+}
+
+export const config = {
+    placeholder: 'placeholder'
+}
